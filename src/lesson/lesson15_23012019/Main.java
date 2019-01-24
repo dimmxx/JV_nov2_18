795218355cc83ca85364ac5c99a906e5469d3af7
dimmxx <dimmxx@gmail.com>
2019-01-24 13:32:25 +0200

    24-01-2019

--- src/lesson/lesson15_23012019/Main.java
+++ src/lesson/lesson15_23012019/Main.java
@@ -5,7 +5,16 @@ import java.sql.*;
 public class Main {
 
 
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/ALKO";
+
+
+    static final String USER = "root";
+    static final String PASS = "";
+
+
     public static void main(String[] args) {
+
 
         try {
             // The newInstance() call is a work around for some
@@ -16,16 +25,11 @@ public class Main {
             // handle the error
         }
 
-
-
-
         Connection conn = null;
         try {
-            conn =
-                    DriverManager.getConnection("jdbc:mysql://localhost/ALKO?" +
-                            "user=root&password=");
 
-            // Do something with the Connection
+
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
 
         } catch (SQLException ex) {
@@ -35,34 +39,32 @@ public class Main {
             System.out.println("VendorError: " + ex.getErrorCode());
         }
 
-        System.out.println("Connection OK");
-
-        Statement st = null;
-        try {
-            st = conn.createStatement();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        String sql = "SELECT * FROM drinks";
-        try {
-            ResultSet rs = st.executeQuery(sql);
-            while (rs.next()){
-                System.out.println(rs.getString(1) + rs.getString(2) + rs.getString(3) + rs.getString(4)+ rs.getString(5));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            st.close();
-            conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+       System.out.println("Connection OK");
+//
+//        Statement st = null;
+//        try {
+//            st = conn.createStatement();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//        String sql = "SELECT * FROM drinks";
+//        try {
+//            ResultSet rs = st.executeQuery(sql);
+//            while (rs.next()) {
+//                System.out.println(rs.getString(1) + rs.getString(2) + rs.getString(3) + rs.getString(4) + rs.getString(5));
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            st.close();
+//            conn.close();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
 
     }
-
 
 }
---
+++ src/lesson/lesson15_23012019/Test.java
@@ -0,0 +1,68 @@
+package lesson.lesson15_23012019;
+
+import java.sql.*;
+
+public class Test {
+
+   // "jdbc:mysql://s3.thehost.ua/itea2?" + "user=helen&password=123456"
+
+
+
+
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/ALKO";
+
+    static final String USER = "root";
+    static final String PASS = "root";
+
+    public static void main(String[] args) {
+
+        try {
+            // The newInstance() call is a work around for some
+            // broken Java implementations
+
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            // handle the error
+        }
+
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+
+       System.out.println("Connection OK...");
+
+        Statement st = null;
+        try {
+            st = conn.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        String sql = "SELECT * FROM drinks";
+        try {
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                System.out.println(rs.getString(1) + " | " + rs.getString(2) + " | " + rs.getString(3)
+                        + " | " + rs.getString(4) + " | " + rs.getString(5));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            st.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
