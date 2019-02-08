b8494a1f8a0049c8214e9ac451f7b545585ac5b7
dimmxx <dimmxx@gmail.com>
2019-02-08 16:52:33 +0200

    08-02-2019

--- src/homework/homework19/FileManagerMain.java
+++ src/homework/homework19/FileManagerMain.java
@@ -5,64 +5,64 @@ import java.util.Scanner;
 
 public class FileManagerMain {
 
+
     public static void main(String[] args) {
 
-        //String testPath = "/home/master/Documents/Test_DIR"; //LINUX
-        String testPath = "/Users/mint/Documents/Test_DIR"; //Mac
-
+        String testPath = "/home/master/Documents/Test_DIR"; //LINUX
+        //String testPath = "/Users/mint/Documents/Test_DIR"; //Mac
 
         Scanner scanner = new Scanner(System.in);
-
         boolean go = true;
 
         while (go) {
             File files = new File(testPath);
+
             System.out.println(files);
 
-
-            for (File file : files.listFiles()) {
-                if (file.isDirectory()) {
-                    System.out.println("[" + file.getName() + "]");
-                }
-            }
-            for (File file : files.listFiles()) {
-                if (file.isFile()) {
-                    System.out.println(file.getName());
-                }
-            }
-
             String move = scanner.nextLine();
-            String moveMod = move.substring(3);
-
-            System.out.println(move);
-            System.out.println(moveMod);
-            System.out.println(move.substring(0 ,2));
 
             if (move.equals("cd ..")) {
                 testPath = files.getParent();
 
-            } else if (move.substring(0 ,3).equals("cd ") & new File(testPath + "/" + moveMod).exists()){
-                testPath += "/" + moveMod;
-        }
+            } else if (move.length() >= 3 && move.substring(0, 3).equals("cd ") &&
+                    new File(testPath + "/" + move.substring(3)).exists()) {
+                testPath += "/" + move.substring(3);
 
-            else if(move.equals("exit")){
+            } else if (move.length() >= 6 && move.substring(0, 6).equals("mkdir ")) {
+                new File(files + "/" + move.substring(6)).mkdir();
+
+            } else if (move.length() >= 2 && move.substring(0, 2).equals("ls")) {
+                printDirList(files);
+
+            } else if (move.length() >= 3 && move.substring(0, 3).equals("cp ")) {
+
+
+
+
+            } else if (move.equals("exit")) {
                 go = false;
+
             } else {
                 System.out.println("-bash: command not found...");
             }
-
-
-
-
-
         }
+
+
     }
 
 
-
-
-
-
+    public static void printDirList(File files) {
+        for (File file : files.listFiles()) {
+            if (file.isDirectory()) {
+                System.out.println("[" + file.getName() + "]");
+            }
+        }
+        for (File file : files.listFiles()) {
+            if (file.isFile()) {
+                System.out.println(file.getName());
+            }
+        }
+    }
 
 
 }
